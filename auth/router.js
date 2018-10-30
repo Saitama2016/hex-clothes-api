d7ff674d0ea22bd311b69ba96873c199c397a125
Vincent Garcia <24626196+Saitama2016@users.noreply.github.com>
2018-10-29 23:33:49 -0700

    Setting up API

---
+++ auth/router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+
+const config = require('../config');
+const router = express.Router();
+
---
+++ users/models.js
@@ -0,0 +1,83 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {type: String, default: ''},
+    lastName: {type: String, default: ''},
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+const outfitSchema = mongoose.Schema({
+    skintone: String,
+    shirt: {
+        type: String,
+        color: String
+    },
+    pants: {
+        type: String,
+        color: String
+    },
+    shoes: {
+        show: Boolean,
+        type: String,
+        color: String
+    }
+});
+
+UserSchema.methods.validatePassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+UserSchema.statics.hashPassword = function(password) {
+    return bcrypt.hash(password, 10);
+};
+
+UserSchema.methods.serialize = function() {
+    return {
+        id: this._id,
+        username: this.username || '',
+        firstName: this.firstName || '',
+        lastName: this.lastName || '',
+        email: this.email,
+    };
+};
+
+outfitSchema.methods.serialize = function() {
+    return {
+        id: this._id,
+        skintone: this.skintone,
+        shirt: {
+            type: this.type,
+            color: this.color
+        },
+        pants: {
+            type: this.type,
+            color: this.color
+        },
+        shoes: {
+            show: this.show,
+            type: this.type,
+            color: this.color
+        }
+    }
+}
+
+const User = mongoose.model('User', UserSchema);
+const Outfit = mongoose.model('Outfit', outfitSchema);
+
+module.exports = { User, Outfit };
